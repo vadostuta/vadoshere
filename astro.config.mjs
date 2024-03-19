a81388f58a09c0436864c6c3e58fae563ea3860a
vadostuta <vadum.vasko@gmail.com>
2024-03-19 22:48:06 +0100

    updat entefliy import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
